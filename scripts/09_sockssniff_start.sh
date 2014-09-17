0ebc666632a38c62ab6bd78defaa4182b6199f51
alor <alberto.ornaghi@gmail.com>
2014-09-17 15:43:15 +0200

    grep is buffered!!! damn it!

--- scripts/09_sockssniff_start.sh
+++ scripts/09_sockssniff_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #title: Socks Sniffing
 ngrep -d eth1 -W byline -t '^(GET|POST) ' 'tcp and port 9150' >> ./data/socks_forms.txt &
-ngrep -d eth1 -W byline -t '^(GET|POST) ' 'tcp and port 9150' | grep -e 'Host:' >> ./data/socks_urls.txt &
+ngrep -d eth1 -W byline -t '^(GET|POST) ' 'tcp and port 9150' >> ./data/socks_urls.txt &
