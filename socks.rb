25ad0230383bb56fbb0dfc6ed68b32fe2f29b28c
alor <alberto.ornaghi@gmail.com>
2014-09-17 10:44:04 +0200

    race condition fix

--- socks.rb
+++ socks.rb
@@ -14,17 +14,41 @@ log "Listening on port tcp/#{localport}..."
 
 MY_IP = "10.0.0.1"
 
-@reply_650OK = "650 NOTICE New control connection opened.\r\n"
+@reply_650_NOTICE = "650 NOTICE New control connection opened.\r\n"
 @reply_250OK = "250 OK\r\n"
 
 @reply_bootstrap_phase = "250-status/bootstrap-phase=NOTICE BOOTSTRAP PROGRESS=100 TAG=done SUMMARY=\"Done\"\r\n"
-@reply_net_listeners_socks = "250-net/listeners/socks=\"#{MY_IP}:9150\" \"127.0.0.1:9150\"\r\n"
+@reply_net_listeners_socks = "250-net/listeners/socks=\"#{MY_IP}:9150\"\r\n"
 @reply_Socks4Proxy = "250 Socks4Proxy\r\n"
 @reply_Socks5Proxy = "250 Socks5Proxy\r\n"
 @reply_HTTPSProxy = "250 HTTPSProxy\r\n"
 @reply_ReachableAddresses = "250 ReachableAddresses\r\n"
 @reply_UseBridges = "250 UseBridges=0\r\n"
 @reply_Bridge = "250 Bridge\r\n"
+
+
+class Event
+  def initialize
+    @lock = Mutex.new
+    @cond = ConditionVariable.new
+  end
+
+  def set
+    log "----- signal set -----"
+    @lock.synchronize do
+      @cond.broadcast
+   end
+  end
+
+  def wait
+    log "----- signal wait -----"
+    @lock.synchronize do
+      @cond.wait(@lock)
+    end
+  end
+end
+
+@event = Event.new
 
 def reply(client, string)
   log " -> #{string}"
@@ -35,6 +59,7 @@ def process_getinfo(client, req)
   if req['status/bootstrap-phase']
     reply client, @reply_bootstrap_phase
     reply client, @reply_250OK
+    @vent.set
   elsif req['net/listeners/socks']
     reply client, @reply_net_listeners_socks
     reply client, @reply_250OK
@@ -73,6 +98,56 @@ def process_getconf(client, req)
   end
 end
 
+def bootstrap_events(client)
+  log "SENDING BOOTSTRAP EVENTS..."
+
+  reply client, "650 NOTICE Bootstrapped 5%: Connecting to directory server.\r\n"
+  sleep 0.1
+  reply client, "650 STATUS_CLIENT NOTICE BOOTSTRAP PROGRESS=5 TAG=conn_dir SUMMARY=\"Connecting to directory server\"\r\n"
+  sleep 0.1
+  reply client, "650 NOTICE Bootstrapped 10%: Finishing handshake with directory server.\r\n"
+  sleep 0.1
+  reply client, "650 STATUS_CLIENT NOTICE BOOTSTRAP PROGRESS=10 TAG=handshake_dir SUMMARY=\"Finishing handshake with directory server\"\r\n"
+  sleep 0.1
+  reply client, "650 NOTICE Bootstrapped 15%: Establishing an encrypted directory connection.\r\n"
+  sleep 0.1
+  reply client, "650 STATUS_CLIENT NOTICE BOOTSTRAP PROGRESS=15 TAG=onehop_create SUMMARY=\"Establishing an encrypted directory connection\"\r\n"
+  sleep 0.1
+  reply client, "650 NOTICE Bootstrapped 20%: Asking for networkstatus consensus.\r\n"
+  sleep 0.1
+  reply client, "650 STATUS_CLIENT NOTICE BOOTSTRAP PROGRESS=20 TAG=requesting_status SUMMARY=\"Asking for networkstatus consensus\"\r\n"
+  sleep 0.1
+  reply client, "650 NOTICE Bootstrapped 25%: Loading networkstatus consensus.\r\n"
+  sleep 0.1
+  reply client, "650 STATUS_CLIENT NOTICE BOOTSTRAP PROGRESS=25 TAG=loading_status SUMMARY=\"Loading networkstatus consensus\"\r\n"
+  sleep 0.1
+  reply client, "650 STATUS_CLIENT NOTICE CONSENSUS_ARRIVED\r\n"
+  sleep 0.1
+  reply client, "650 NOTICE Bootstrapped 45%: Asking for relay descriptors.\r\n"
+  sleep 0.1
+  reply client, "650 STATUS_CLIENT NOTICE BOOTSTRAP PROGRESS=45 TAG=requesting_descriptors SUMMARY=\"Asking for relay descriptors\"\r\n"
+  sleep 0.1
+  reply client, "650 NOTICE We now have enough directory information to build circuits.\r\n"
+  sleep 0.1
+  reply client, "650 STATUS_CLIENT NOTICE ENOUGH_DIR_INFO\r\n"
+  sleep 0.1
+  reply client, "650 NOTICE Bootstrapped 80%: Connecting to the Tor network.\r\n"
+  sleep 0.1
+  reply client, "650 STATUS_CLIENT NOTICE BOOTSTRAP PROGRESS=80 TAG=conn_or SUMMARY=\"Connecting to the Tor network\"\r\n"
+  sleep 0.1
+  reply client, "650 NOTICE Bootstrapped 90%: Establishing a Tor circuit.\r\n"
+  sleep 0.1
+  reply client, "650 STATUS_CLIENT NOTICE BOOTSTRAP PROGRESS=90 TAG=circuit_create SUMMARY=\"Establishing a Tor circuit\"\r\n"
+  sleep 0.1
+  reply client, "650 NOTICE Tor has successfully opened a circuit. Looks like client functionality is working.\r\n"
+  sleep 0.1
+  reply client, "650 NOTICE Bootstrapped 100%: Done.\r\n"
+  sleep 0.1
+  reply client, "650 STATUS_CLIENT NOTICE BOOTSTRAP PROGRESS=100 TAG=done SUMMARY=\"Done\"\r\n"
+  sleep 0.1
+  reply client, "650 STATUS_CLIENT NOTICE CIRCUIT_ESTABLISHED\r\n"
+end
+
 def process_request(client)
   loop do
     begin
@@ -85,12 +160,17 @@ def process_request(client)
         process_getinfo(client, req)
       elsif req.start_with? 'SETEVENTS'
         reply client, @reply_250OK
-        log "SETEVENTS !!!"
-        #reply client, @reply_650OK
-        return
+        @event.wait
+        bootstrap_events(client)
+        while(true)
+          @event.wait
+          log "EVENT NOTICE..."
+          reply client, @reply_650_NOTICE
+        end
       elsif req.start_with? 'SIGNAL NEWNYM'
         reply client, @reply_250OK
         log "NEWNYM !!!"
+        @event.set
         return
       elsif req.start_with? 'TAKEOWNERSHIP'
         reply client, @reply_250OK
