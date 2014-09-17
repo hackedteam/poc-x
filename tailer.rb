10dfda3688d050ceec00b3288e2adfebd928f46a
topac <topac@hackingteam.com>
2014-09-17 15:01:36 +0200

    format_params

--- tailer.rb
+++ tailer.rb
@@ -3,6 +3,7 @@ class Tailer
   attr_reader :unique
   attr_reader :match
   attr_reader :filename
+  attr_reader :format_params
 
   # &block is called async
   def initialize(filename, options = {}, &block)
@@ -11,6 +12,7 @@ class Tailer
     @path = File.join(self.class.base_path, filename)
     @unique = options[:unique]
     @match = Regexp.new(options[:match], true) if options[:match]
+    @format_params = options[:format_params] || options[:formatParams]
 
     create_if_missing
 
@@ -19,6 +21,7 @@ class Tailer
         @last_line = line
 
         if !match or (match and line =~ match)
+          line = " > " + line.gsub!("&", "\n> ") if format_params and line =~ /\=.+(\&.+\=)+/
           @block.call(line.strip, filename)
         end
       end
