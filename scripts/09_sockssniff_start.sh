4aedd99d16a2c1090f97061633658833415e4eb9
alor <alberto.ornaghi@gmail.com>
2014-09-18 09:30:47 +0200

    cosmetics

--- scripts/09_sockssniff_start.sh
+++ scripts/09_sockssniff_start.sh
@@ -2,3 +2,5 @@
 #title: Socks Sniffing
 ngrep -d eth1 -W byline -t '^(GET|POST) ' 'tcp and port 9150' >> ./data/socks_forms.txt &
 ngrep -d eth1 -W byline -t '^(GET|POST) ' 'tcp and port 9150' >> ./data/socks_urls.txt &
+echo "Sniffing started..." >> ./data/socks_forms.txt
+echo "Sniffing started..." >> ./data/socks_urls.txt
