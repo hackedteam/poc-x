311c3558311e933037d48be3c4d467e369205862
alor <alberto.ornaghi@gmail.com>
2014-09-26 15:05:21 +0200

    drop non intercepted https connections

--- scripts/07_proxy443_start.sh
+++ scripts/07_proxy443_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #title: Intercept HTTPS
 iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 443 -j REDIRECT --to-port 8080
+iptables -A FORWARD -p tcp --dport 443 -j REJECT --reject-with tcp-reset
--- scripts/07_proxy443_stop.sh
+++ scripts/07_proxy443_stop.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 iptables -t nat -D PREROUTING -i eth1 -p tcp --dport 443 -j REDIRECT --to-port 8080
+iptables -D FORWARD -p tcp --dport 443 -j REJECT --reject-with tcp-reset
