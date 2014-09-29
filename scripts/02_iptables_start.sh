938c7c57d65a6a24c9e0049616df760f3a5d3672
alor <alberto.ornaghi@gmail.com>
2014-09-29 14:44:37 +0200

    permit tor download

--- scripts/02_iptables_start.sh
+++ scripts/02_iptables_start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 #title: Iptables Routing and NAT
 echo "1" > /proc/sys/net/ipv4/ip_forward
-iptables -A FORWARD -i eth0 -o eth1 -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
+iptables -A FORWARD -i eth1 -p tcp -d 38.229.72.14 -j ACCEPT
+iptables -A FORWARD -i eth1 -p tcp -d 38.229.72.16 -j ACCEPT
+iptables -A FORWARD -i eth1 -p tcp -d 154.35.132.70 -j ACCEPT
+iptables -A FORWARD -i eth1 -p tcp -d 82.195.75.101 -j ACCEPT
+iptables -A FORWARD -i eth1 -p tcp -d 86.59.30.40 -j ACCEPT
+iptables -A FORWARD -i eth1 -p tcp -d 93.95.227.222 -j ACCEPT
+
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -t nat -A PREROUTING -i eth1 -p tcp -d 38.229.72.14 -j RETURN
 iptables -t nat -A PREROUTING -i eth1 -p tcp -d 38.229.72.16 -j RETURN
--- scripts/02_iptables_stop.sh
+++ scripts/02_iptables_stop.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 echo "0" > /proc/sys/net/ipv4/ip_forward
-iptables -D FORWARD -i eth0 -o eth1 -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -D FORWARD -i eth1 -o eth0 -j ACCEPT
+iptables -D FORWARD -i eth1 -p tcp -d 38.229.72.14 -j ACCEPT
+iptables -D FORWARD -i eth1 -p tcp -d 38.229.72.16 -j ACCEPT
+iptables -D FORWARD -i eth1 -p tcp -d 154.35.132.70 -j ACCEPT
+iptables -D FORWARD -i eth1 -p tcp -d 82.195.75.101 -j ACCEPT
+iptables -D FORWARD -i eth1 -p tcp -d 86.59.30.40 -j ACCEPT
+iptables -D FORWARD -i eth1 -p tcp -d 93.95.227.222 -j ACCEPT
+
 iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
 iptables -t nat -D PREROUTING -i eth1 -p tcp -d 38.229.72.14 -j RETURN
 iptables -t nat -D PREROUTING -i eth1 -p tcp -d 38.229.72.16 -j RETURN
